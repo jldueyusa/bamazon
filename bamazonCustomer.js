fefb4c1d6b4f4a261f722623b600dfaffad44922
Jennifer Duey <jlduey@gmail.com>
2019-06-11 19:17:51 -0400

    function to manage connection query to mysql

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,3 +14,16 @@ port: 3306
 //call it
 connection.connect();
 
+//function will manage connection query to mysql
+
+var display = function() {
+    connection.query("SELECT * FROM products",function(err,res){
+        if(err) throw err;
+        console.log("-----------------------------");
+        console.log("     Welcome to Bamazon      ");
+        console.log("-----------------------------");
+        console.log("");
+        console.log("Find Your Product Below");
+        console.log("");
+    });
+  
