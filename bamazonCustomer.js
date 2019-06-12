8ceb454f45704b307d8fa61d4ff815f1014a53bf
Jennifer Duey <jlduey@gmail.com>
2019-06-12 17:01:50 -0400

    I accidentally marked my other homework file as private-using this git repository

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,43 +1,78 @@
-//connect require packages
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var Table = require("cli-table2");
+//require packages
+var inquirer  = require('inquirer');
+var mysql = require('mysql');
 
-//connection variable to connect to mysql db
 var connection = mysql.createConnection({
-host: "localhost",
-user: "root",
-password: "MmSj.01!",
-database: "bamazon_db",
-port: 3306
-})
-//call it
+    host: 'localhost',
+    user: 'root',
+    password: 'MmSj.01!',
+    database: 'bamazon'
+});
+
 connection.connect();
 
-//function will manage connection query to mysql
+start()
 
-var display = function() {
-    connection.query("SELECT * FROM products",function(err,res){
-        if(err) throw err;
-        console.log("-----------------------------");
-        console.log("     Welcome to Bamazon      ");
-        console.log("-----------------------------");
-        console.log("");
-        console.log("Find Your Product Below");
-        console.log("");
-    });
-    //table variable connect to cli table
-    var table = new Table({
-        head: ["item_id", "product_name","price"],
-        colWidths: [12, 50, 8],
-        colAligns: ["center", "left", "right"],
-        style: {
-            head: ["aqua"],
-            compact: true
+function start (){
+    connection.query('SELECT * FROM products', function (error, res) {
+        if (error) throw error;
+        // console.log(res);
+        res.forEach(row => {
+            console.log(`Id: ${row.item_id} Name: ${row.product_name} Price: ${row.price}\n` )
+        });
+        askQuestions()
+
+        // connection.end()
+    })
+}
+
+function askQuestions() {
+    inquirer.prompt([
+        {
+            message: "Please type in the product id you would like to order.",
+            type: "input",
+            name: "prodId"
+        },
+        {
+            message: "how many of this item would you like to purchase",
+            type: "input",
+            name: "prodQty"
         }
+    ]).then(function (ans) {
+        var prodId = ans.prodId;
+        var prodQty = ans.prodQty;
+        // var allProd = getAllProd();
+        withdrawProd(prodId, prodQty)
     });
-    //create for loop
-    for (var = i; i < res.length; i++){
-        table.push([res[i].item_id, res[i].product_name, res[i].price]);
+}
+
+function withdrawProd(prodId, prodQty) {
+  connection.query('SELECT * FROM products', function (error, res) {
+    if (error) throw error;
+    var prod;
+    // console.log(res);
+    for(var i = 0; i < res.length; i++){
+      if(res[i].item_id == prodId){
+        prod = res[i]
+      }
     }
+    console.log(prod, "prod was found")
+      if(prod.stock_quantity >= prodQty){
+        orderComplete(prod, prodId, prodQty)
+        connection.end()
+      }else{
+        console.log("sorry the order has been cancled, there was insuffecent stock of this purchase")
+        connection.end()
+      }
+  })
 };
+function orderComplete (prodObj, prodId, prodQty) {
+  var newQuantity = prodObj.stock_quantity - prodQty;
+  var productSales = prodObj.price * prodQty;
+  var queryOne = "UPDATE products SET stock_quantity = ? where ?";
+  var queryTwo = "UPDATE products SET product_sales = ? where ?";
+  connection.query(queryOne,[newQuantity, {item_id: prodId}], function (error, res) {
+  })
+  connection.query(queryTwo, [productSales, { item_id: prodId }], function (error, res) {
+  })
+}
