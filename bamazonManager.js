22d9518fc9e76c79da9bc4b8d13b8eeb2701ad90
Jennifer Duey <jlduey@gmail.com>
2019-06-12 23:23:21 -0400

    adding an image for readme

--- bamazonManager.js
+++ bamazonManager.js
@@ -8,3 +8,33 @@ var connection = mysql.createConnection({
     password: 'MmSj.01!',
     database: 'bamazon'
 });
+connection.connect();
+askQuestions()
+function askQuestions() {
+  inquirer.prompt([
+    {
+      message: "What would you like to do?",
+      type: "list",
+      name: "managerAction",
+      choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+    }
+  ]).then(function (ans) {
+    switch (ans.managerAction) {
+      case "View Products for Sale":
+        viewProducts()
+        break;
+      case "View Low Inventory":
+        viewLowInventory()
+        break;
+      case "Add to Inventory":
+        selectInventory()
+        break;
+      case "Add New Product":
+        addProduct()
+        break;
+      default:
+
+        break;
+    }
+  });
+}
